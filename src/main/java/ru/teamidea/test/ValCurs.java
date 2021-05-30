9f06d2bc939a6c6eafb447982409e3a1b4c1de44
alexeycheshkov <alexeychehskov@gmail.com>
2021-05-30 12:03:12 +0500

    1. Correct pom.xml (maven package).

--- src/main/java/ru/teamidea/test/ValCurs.java
+++ src/main/java/ru/teamidea/test/ValCurs.java
@@ -9,7 +9,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
